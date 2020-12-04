d813c60cbd7bc5ba71235ff750e99054f74e6831
Rony Aldhea Dwi Cahya <radc.boy@gmail.com>
2020-12-04 19:02:00 +0700

    add file Execute.java add file PageObject to define WebDriver add file LoginPage.java to open web url update selenium version

---
+++ src/main/java/login/LoginPage.java
@@ -0,0 +1,13 @@
+package login;
+
+import org.openqa.selenium.WebDriver;
+import pages.PageObject;
+
+public class LoginPage extends PageObject {
+
+    public void openPage(){
+
+        getDriver().get("https://www.google.com/");
+    }
+
+}
---
+++ src/main/java/pages/PageObject.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageObject {
+
+   public static WebDriver driver = new ChromeDriver();
+
+   public WebDriver getDriver() {
+       return this.driver;
+   }
+
+
+}
