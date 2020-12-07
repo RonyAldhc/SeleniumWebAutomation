7e1224f401ae3f559dce7b00ceebcf197668a508
Rony Aldhea Dwi Cahya <radc.boy@gmail.com>
2020-12-07 13:35:58 +0700

    add scenario using cucumber add method to stepdefinition

---
+++ src/main/java/stepdefinition/LoginSentinelSteps.java
@@ -0,0 +1,19 @@
+package stepdefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSentinelSteps {
+    @Given("Open Website Sentinel")
+    public void openWebsiteSentinel() {
+    }
+
+    @When("User input username {string} and password {string}")
+    public void userInputUsernameAndPassword(String arg0, String arg1) {
+    }
+
+    @Then("User success to login")
+    public void userSuccessToLogin() {
+    }
+}
