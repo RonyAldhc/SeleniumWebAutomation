443ddcc6ddc7e5e3e49e9bfbddd95a6abaf7ff07
Rony Aldhea Dwi Cahya <radc.boy@gmail.com>
2020-12-07 14:02:15 +0700

    add logic to input username and password

--- src/main/java/pages/LoginSentinel.java
+++ src/main/java/pages/LoginSentinel.java
@@ -1,8 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class LoginSentinel extends PageObject{
 
     public void openPage (){
         getDriver().get("https://kraken-biller.sumpahpalapa.com/");
+    }
+
+    public void inputCredentials (String userName, String password) {
+        WebElement username = getDriver().findElement(By.className("input-group mb-3"));
+        username.sendKeys(userName);
+
+        WebElement Password = getDriver().findElement(By.className("input-group mb-1"));
+        Password.sendKeys(password);
     }
 }
